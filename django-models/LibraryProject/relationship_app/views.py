ce46b650f61322887bed68bafa412bf7f39e1a3b
flameloaded <samuelvictor747futa@gmail.com>
2025-07-23 12:04:45 +0100

    add view and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library  # required exact line
 
 # Function-Based View: List all books
 def list_books(request):
@@ -12,4 +13,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
