9ae5c615ed1e31ca6b921bb8995aa0c46cbf064c
flameloaded <samuelvictor747futa@gmail.com>
2025-07-23 14:18:43 +0100

    permission

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -31,12 +31,23 @@ class Author(models.Model):
         return self.name
 
 
+from django.db import models
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
+
 
 
 class Library(models.Model):
