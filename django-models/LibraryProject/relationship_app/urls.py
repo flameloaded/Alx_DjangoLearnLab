fcd903849abb094bfa450add5137c97a51424ea8
flameloaded <samuelvictor747futa@gmail.com>
2025-07-23 13:49:31 +0100

    added role

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
+
 from .views import profile_view
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
@@ -12,10 +10,9 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin-role/', admin_view, name='admin-role'),
-    path('librarian-role/', librarian_view, name='librarian-role'),
-    path('member-role/', member_view, name='member-role'),
-    path('profile/', profile_view, name='profile'),
+    path('member/', views.member_view, name='member_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('admin/', views.admin_view, name='admin_view'),
 ]
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,9 +8,39 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm  # ✅ Add this
 from django.views.generic.detail import DetailView
 from .models import Library, Book
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from .models import UserProfile
+from django.shortcuts import render
+
+
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+from .models import UserProfile
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 
 # Function-Based View: List all books
