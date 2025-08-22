0d86d31d7b56ee291fd632ba930e7bc595f018a3
flameloaded <samuelvictor747futa@gmail.com>
2025-08-22 13:00:51 +0100

    api set up

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,25 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # <-- satisfies serializers.CharField()
+    password2 = serializers.CharField(write_only=True) # confirm password field
 
-class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        Token.objects.create(user=user)
+        validated_data.pop('password2')
+        # <-- satisfies get_user_model().objects.create_user()
+        user = User.objects.create_user(**validated_data)
         return user
