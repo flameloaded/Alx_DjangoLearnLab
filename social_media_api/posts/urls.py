f2621362f596cb00cbaffe057516f4d88a111976
flameloaded <samuelvictor747futa@gmail.com>
2025-08-23 13:39:23 +0100

    add follow and unfollow

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 from django.urls import path
+from . import views
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -8,5 +9,7 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
+    path('<int:pk>/like/', views.LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike-post'),
 ]
 urlpatterns = router.urls
